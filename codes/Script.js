0388d7878acc681b8da252fd66df173c87816451
Mikcsu <miika.puominen@gmail.com>
2023-10-22 21:57:36 +0300

    melekkee valamis

---
+++ codes/Script.js
@@ -0,0 +1,65 @@
+
+
+document.getElementById('sr').addEventListener('click', function(){
+    alert('makiia');
+});
+document.getElementById('aram').addEventListener('click', function(){
+    alert('aah an enjoyer');
+});
+document.getElementById('tft').addEventListener('click', function(){
+    alert('ewww barf');
+});
+
+
+const textBox = document.getElementById('champ');
+const teemo = document.getElementById('temo');
+teemo.src = 'pictures/teemo.jpg';
+const yasuo = document.getElementById('yas');
+yasuo.src = 'pictures/yasuo.jpg';
+
+// Add an input event listener to the text box
+textBox.addEventListener('keypress', function() {
+    if(event.key === 'Enter'){
+    // Get the text entered in the text box
+    const enteredText = textBox.value.toLowerCase(); // Convert to lowercase for case-insensitivity
+    
+
+
+    if (enteredText === 'teemo') {
+        // Display an alert if the entered text is "hello"
+        alert('You have no SOUL!');
+        teemo.style.display = 'block';
+    }
+    else if(enteredText === 'yasuo'){
+        yasuo.style.display = 'block';
+    }
+    else{
+        alert('no makiia');
+    }
+}
+});
+
+const kysely = document.getElementById('kysely');
+
+document.getElementById('alert-button').addEventListener('click', function() {
+    kysely.style.display = 'block';
+});
+
+const quizForm = document.getElementById('kysely');
+const submitButton = document.getElementById('submit-button');
+const scoreElement = document.getElementById('score');
+
+submitButton.addEventListener('click', function() {
+    let score = 0;
+
+    // Get the selected answers and calculate the score
+    const answers = quizForm.elements;
+    for (let i = 0; i < answers.length; i++) {
+        if (answers[i].type === 'radio' && answers[i].checked) {
+            score += parseInt(answers[i].value);
+        }
+    }
+
+    // Display the score
+    scoreElement.textContent = score;
+});
